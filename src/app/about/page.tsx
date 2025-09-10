732e1bc1596b078c9068eb6d1ae8e3e53bfe5142
Juwon Hwang <juwonhwang0411@gmail.com>
2025-09-10 15:18:26 +0900

    feat: add About, Blog, and Home pages with Profile component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Profile from "./profile";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Profile />
+    </div>
+  );
+}
---
+++ src/app/about/profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>내 프로필은 이래요</h1>
+    </div>
+  );
+}
