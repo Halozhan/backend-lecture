b5e4ed896c690eb1ae71f76c6396e4e948e97645
Juwon Hwang <juwonhwang0411@gmail.com>
2025-09-22 13:34:21 +0900

    CSR 코드와 SSR 코드 분리

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,27 +1,10 @@
-"use client";
 import { Metadata } from "next";
-import { useState } from "react";
+import Counter from "./counter";
 
 export const metadata: Metadata = {
   title: "Counter Page",
 };
 
 export default function CounterPage() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <div
-        onClick={() => setCount(count + 1)}
-        style={{
-          color: "cyan",
-          border: "1px solid cyan",
-          padding: 8,
-          display: "inline-block",
-        }}
-      >
-        증가!
-      </div>
-    </div>
-  );
+  return <Counter />;
 }
