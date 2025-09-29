505053ee9f2fb05f216a8c311b71b269d13bcd66
Juwon Hwang <juwonhwang0411@gmail.com>
2025-09-29 13:11:16 +0900

    내비게이션 링크 활성화 상태 관리 및 스타일 시트 추가

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { Metadata } from "next";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
 
-export const metadata: Metadata = {
-  title: "Auth Page",
-  description: "This is Auth page",
-};
+// export const metadata: Metadata = {
+//   title: "Auth Page",
+//   description: "This is Auth page",
+// };
 
 const navLinks = [
   {
@@ -26,11 +29,21 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
     <div>
       {navLinks.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+
         return (
-          <Link key={link.name} href={link.href}>
+          <Link
+            key={link.name}
+            href={link.href}
+            className={isActive ? "font-bold mr-4" : "text-green-200 mr-4"}
+          >
             {link.name}
           </Link>
         );
