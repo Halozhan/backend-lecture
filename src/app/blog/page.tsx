52cd4d6fff454749a95099a1036d26cc994d2862
Juwon Hwang <juwonhwang0411@gmail.com>
2025-09-29 13:52:21 +0900

    setTimeout을 사용하여 로딩 2초 구현

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
-export default function BlogPage() {
+export default async function BlogPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <div>
       <h1>Blog Page</h1>
