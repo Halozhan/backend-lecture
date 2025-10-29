08daebed8e844e76f6ab2d6b6c1ba16b3430a89f
Juwon Hwang <juwonhwang0411@gmail.com>
2025-10-29 15:16:41 +0900

    테마 쿠키 설정

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request: Request) {
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.rewrite(new URL("/", request.url));
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
+  response.headers.set("custom-header", "custom-value");
+  return response;
 }
-
-// export const config = {
-//   matcher: "/profile",
-// };
