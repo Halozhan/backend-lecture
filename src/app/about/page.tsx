ef048cd779767e49eb0f47a6227bd4a222798890
Juwon Hwang <juwonhwang0411@gmail.com>
2025-11-10 13:02:09 +0900

    클라이언트에서만 작동하는 코드

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useState } from "react";
+
 export default function AboutPage() {
-  // 서버 콘솔에서 처리되는 로직이다.
+  // 클라이언트에서 처리되는 로직이다.
   console.log("About Page Rendered");
+  const [name, setName] = useState("Lee");
+  console.log("Client Side Name:", name);
   return (
     <div>
       <h1>About Page</h1>
+      {
+        /* 클라이언트 사이드에서만 동작하는 부분 */
+        <button onClick={() => setName("Kim")}>Change Name</button>
+      }
+      <p>Name: {name}</p>
     </div>
   );
 }
