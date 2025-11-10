deb30278aab9cb025007148c28d7b46b01fe337b
Juwon Hwang <juwonhwang0411@gmail.com>
2025-11-10 14:17:07 +0900

    정적 페이지를 동적 페이지로 만들기. 쿠키 사용

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,14 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+  const cookie = await cookies();
+  const theme = cookie.get("theme")?.value ?? "light";
+  // 브라우저가 자동으로 시스템의 테마. dark theme를 설정해주는 것 같다.
   return (
     <div>
       <h1>About Page</h1>
       <p>{new Date().toLocaleTimeString()}</p>
+      <p>Current theme: {theme}</p>
     </div>
   );
 }
